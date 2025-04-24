fed4eb5e310dcc6175b0de61667e26c77188b085
EigenFunction271 <brendan02beh@gmail.com>
2025-04-24 20:49:45 +0800

    more fixes

--- src/script.js
+++ src/script.js
@@ -375,7 +375,7 @@ function animateCursor() {
   cursorX += (cursorTargetX - cursorX) * 0.1;
   cursorY += (cursorTargetY - cursorY) * 0.1;
   
-  customCursor.style.transform = `translate(${cursorX}px, ${cursorY}px) translate(-8px, -8px)`;
+  customCursor.style.transform = `translate(${cursorX}px, ${cursorY}px) translate(-4px, -4px)`;
   
   requestAnimationFrame(animateCursor);
 }
